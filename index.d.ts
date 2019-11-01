68db974a8c426b5c804cf62b98410ea72f23e00b
Chris Talman <cewe.mail@gmail.com>
2019-11-01 11:49:05 +0000

    Fixed package name scope.

--- index.d.ts
+++ index.d.ts
@@ -7,7 +7,7 @@ import Moment from 'moment';
 type ReturnType <T> = T extends (...args: any[]) => infer R ? R : any;
 type PromiseValue <T> = T extends Promise<infer V> ? V : never;
 
-declare module '@ChrisTalman/rethink-utilities'
+declare module '@chris-talman/rethink-utilities'
 {
 	/** Runs query and returns result if successful, otherwise reattempts several times until throwing an exception with the final error. */
 	export function run <GenericQuery extends RQuery> ({query, options}: {query: GenericQuery, options: RunUtilityOptions}): PromiseValue<ReturnType<GenericQuery['run']>>;
